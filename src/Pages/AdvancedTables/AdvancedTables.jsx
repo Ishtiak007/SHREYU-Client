a7095cddeaed31a40c967ff158d0d1bb5ef6e4d8
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-04-29 14:56:18 +0600

    MULTIPLE ROW SELECTION table added to the AdvancedTables

--- src/Pages/AdvancedTables/AdvancedTables.jsx
+++ src/Pages/AdvancedTables/AdvancedTables.jsx
@@ -1,3 +1,4 @@
+import MultipleRowSelection from "../Tables/MultipleRowSelection/MultipleRowSelection";
 import PaginationSortComp from "../Tables/PaginationSortTable/PaginationSortComp";
 import SearchTables from "../Tables/SearchTables/SearchTables";
 
@@ -8,6 +9,7 @@ const AdvancedTables = () => {
         <div className="space-y-10">
             <PaginationSortComp></PaginationSortComp>
             <SearchTables></SearchTables>
+            <MultipleRowSelection></MultipleRowSelection>
         </div>
     );
 };
